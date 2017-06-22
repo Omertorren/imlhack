bc886fda4e078eba73fb04659cdbe110a5dc992f
Omer <Omertorrents@gmail.com>
2017-06-22 18:46:11 +0300

    :D

--- data/getData.py
+++ data/getData.py
@@ -2,3 +2,7 @@ def getData():
     haaretz = open("haaretz.csv", "r").read().split("\n")
     israelHayom = open("israelhayom.csv","r").read().split("\n")
     return haaretz, israelHayom
+
+
+def getWord():
+    return open("a.txt","r").read().split(',')
