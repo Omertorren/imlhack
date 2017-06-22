ccb5932fc64b130c6e407af575dea3d66d7f5269
Jonathanweiss <jonathan.weiss1@mail.huji.ac.il>
2017-06-22 22:59:24 +0300

    Merge branch 'master' of /Users/yonatanweiss/heavy/GIT/imlhack with conflicts.

--- simplifiedBag.py
+++ simplifiedBag.py
@@ -43,7 +43,7 @@ def normalize_weights(weight):
 
 def compute_sum(sentence, dictionary):
     nsentence = cleanse_raw([sentence])
-    nsentence = normalize_words(ignore_irrelevant_words(get_words(nsentence)))
+    nsentence = normalize_words((get_words(nsentence)))
     sum = 0
     for wrd in nsentence:
         if wrd in dictionary:
@@ -55,23 +55,37 @@ def compute_sum(sentence, dictionary):
 
 def compute_length(sentence):
     nsentence = cleanse_raw([sentence])
-    nsentence = normalize_words(ignore_irrelevant_words(get_words(nsentence)))
+    nsentence = normalize_words((get_words(nsentence)))
     return len(sentence)
 
 
-if __name__ == '__main__':
+def avarage_length(data1, data2):
+    len1 = 0
+    len2 = 0
+    for sentence in data1:
+        len1 += len(normalize_words(get_words(cleanse_raw([sentence]))))
+    len1 = len1 / len(data1)
+
+    for sentence in data2:
+        len2 += len(normalize_words(get_words(cleanse_raw([sentence]))))
+    len2 = len2 / len(data2)
+    return len1, len2
+
+
+def j_weights():
     stemmer = SnowballStemmer("english")
     word_list = getWord()
-    word_list = normalize_words(ignore_irrelevant_words(word_list))
+    word_list = normalize_words((word_list))
 
     weight = dict()
     for word in word_list:
         weight[word] = 0
 
     haaretz_list, israel_hayom_list = getData()
+    print(len(haaretz_list), len(israel_hayom_list))
     # cleaning out bad words.
-    haaretz_list = ignore_irrelevant_words(cleanse_raw(haaretz_list))
-    israel_hayom_list = ignore_irrelevant_words(cleanse_raw(israel_hayom_list))
+    haaretz_list = (cleanse_raw(haaretz_list))
+    israel_hayom_list = (cleanse_raw(israel_hayom_list))
 
     # gathering validation
     h_validate = haaretz_list[int((len(haaretz_list) / 10) * 8):]
@@ -84,33 +98,76 @@ if __name__ == '__main__':
     counter_for_dictionary(i_train, 1, weight)
     counter_for_dictionary(h_train, -1, weight)
     normalize_weights(weight)
+    ilen, hlen = avarage_length(i_train, h_train)
+    return weight, ilen, hlen
 
-    h_count = 0
-    h_half = 0
-    h_wrong = 0
-    for i in h_validate:
-        val = compute_sum(i, weight)[0]
-        if val < 0.5:
-            h_count += 1
-        elif val == 0.5:
-            h_half += 1
-        elif val > 0.5:
-            h_wrong += 1
 
-    i_count = 0
-    i_half = 0
-    i_wrong = 0
-    for j in i_validate:
-        val = compute_sum(j, weight)[0]
-        if val > 0.5:
-            i_count += 1
-        elif val == 0.5:
-            i_half += 1
-        elif val < 0.5:
-            i_wrong += 1
+def compute_len_score(sentence, len1, len2):
 
-    for j in i_validate:
 
-    print("rights  |overallsize |wrongs|   non-dicidable")
-    print(i_count, "   ", len(i_validate), "         ", i_wrong, "    ", i_half)
-    print(h_count, "   ", len(h_validate), "         ", h_wrong, "    ", h_half)
+
+# stemmer = SnowballStemmer("english")
+# word_list = getWord()
+# word_list = normalize_words((word_list))
+#
+# weight = dict()
+# for word in word_list:
+#     weight[word] = 0
+#
+# haaretz_list, israel_hayom_list = getData()
+# print(len(haaretz_list), len(israel_hayom_list))
+# # cleaning out bad words.
+# haaretz_list = (cleanse_raw(haaretz_list))
+# israel_hayom_list = (cleanse_raw(israel_hayom_list))
+#
+# # gathering validation
+# h_validate = haaretz_list[int((len(haaretz_list) / 10) * 8):]
+# i_validate = israel_hayom_list[int((len(israel_hayom_list) / 10) * 8):]
+# # train set
+# h_train = haaretz_list[:int((len(haaretz_list) / 10) * 8)]
+# i_train = israel_hayom_list[:int((len(israel_hayom_list) / 10) * 8)]
+#
+# # starting with 1 for haaretz today.
+# counter_for_dictionary(i_train, 1, weight)
+# counter_for_dictionary(h_train, -1, weight)
+# normalize_weights(weight)
+#
+# h_count = 0
+# h_half = 0
+# h_wrong = 0
+# for i in h_validate:
+#     val = compute_sum(i, weight)[0]
+#     if val < 0.5:
+#         h_count += 1
+#     elif val == 0.5:
+#         h_half += 1
+#     elif val > 0.5:
+#         h_wrong += 1
+#
+# i_count = 0
+# i_half = 0
+# i_wrong = 0
+# for j in i_validate:
+#     val = compute_sum(j, weight)[0]
+#     if val > 0.5:
+#         i_count += 1
+#     elif val == 0.5:
+#         i_half += 1
+#     elif val < 0.5:
+#         i_wrong += 1
+
+
+if __name__ == '__main__':
+    dicter, len1, len2 = j_weights()
+
+    # for j in i_validate:
+    # print("rights  |overallsize |wrongs|   non-dicidable")
+    # print(i_count, "   ", len(i_validate), "         ", i_wrong, "    ", i_half)
+    # print(h_count, "   ", len(h_validate), "         ", h_wrong, "    ", h_half)
+
+    ###### avg length  :
+    # for j in i_train:
+    #     print()
+
+    ilen, hlen = avarage_length(i_train, h_train)
+    print(ilen, hlen)
